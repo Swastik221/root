e0fe2cf42450666819f66064320cd8dc93f0cc71
Swastik Tiwari <swastiktiwari9336414294@gmail.com>
2026-07-07 14:41:36 +0530

    path changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // or just remove the base line entirely
   plugins: [react()],
-  base: '/root/',
 })
